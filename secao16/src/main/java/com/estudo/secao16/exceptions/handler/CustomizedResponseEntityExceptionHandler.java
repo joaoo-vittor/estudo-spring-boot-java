0547c9767efd13e1ead12d62eb64af31fa2a7b68
joaoo-vittor <joaoo.vittor007@gmail.com>
2022-09-12 00:47:55 -0300

    Autenticancao com JWT e Spring Security

--- secao16/src/main/java/com/estudo/secao16/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
+++ secao16/src/main/java/com/estudo/secao16/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
@@ -11,6 +11,7 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.estudo.secao16.exceptions.ExceptionResponse;
+import com.estudo.secao16.exceptions.InvalidJwtAuthenticationException;
 import com.estudo.secao16.exceptions.RequiredObjectIsNullException;
 import com.estudo.secao16.exceptions.ResourceNotFoundException;
 
@@ -53,6 +54,20 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 				request.getDescription(false));
 		
 		return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(InvalidJwtAuthenticationException.class)
+	public final ResponseEntity<ExceptionResponse> handleInvalidJwtAuthenticationExceptions(
+		Exception ex, WebRequest request
+	) {
+		
+		ExceptionResponse exceptionResponse = new ExceptionResponse(
+			new Date(),
+			ex.getMessage(),
+			request.getDescription(false)
+		);
+		
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.FORBIDDEN);
 	}
 
 }
